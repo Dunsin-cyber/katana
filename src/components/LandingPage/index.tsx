768aaf46b8af2c7d38c5f466e207fc077eab7e7e
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-12-01 08:28:07 +0100

    tryging to fix prerendering page error on vercel

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-// import { AppContext } from "../../Context";
 import Footer from "../Footer";
 import Why from "./Why";
 import Trade from "./Trade";
@@ -9,15 +7,6 @@ import Stats from "./Stats";
 import Hero from "./Hero";
 
 function LandingPage() {
-  const [showModal, setShowModal] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const navigate = useRouter();
-  const words_ = ["crowd funds", "manage NFT’s", "manage your balances"];
-  const handleClick = async () => {
-    navigate.push("profile");
-  };
-
   return (
     <div className="bg-custom-gradient">
       <Hero />
