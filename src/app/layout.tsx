d6cd88a16781ac22e00fe5f574547d8b5c8033d5
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-12-01 07:49:06 +0100

    tryging to fix prerendering page error on vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
-          <Toaster />
+          {/* <Toaster /> */}
           {children}
         </Provider>
       </body>
