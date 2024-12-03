85fbe0749f4dbc369dda088990191f6990a66fdb
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-12-03 12:22:27 +0100

    using dynamic import

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React from "react";
+import dynamic from "next/dynamic";
 import Footer from "../Footer";
-import Why from "./Why";
+// import Why from "./Why";
 import Trade from "./Trade";
 import Stats from "./Stats";
-import Hero from "./Hero";
+// import Hero from "./Hero";
+
+const Hero = dynamic(() => import("./Hero"), { ssr: false });
+const Why = dynamic(() => import("./Why"), { ssr: false });
 
 function LandingPage() {
   if (typeof window === "undefined") {
